c913d3bb3aac3f10957c69e07fe731c584774836
sanitgit <sanits212@gmail.com>
2026-03-12 14:01:07 +0800

    changes made

--- main.tf
+++ main.tf
@@ -1,9 +1,13 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
 data "aws_ami" "ubuntu" {
   most_recent = true
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 
   filter {
@@ -11,11 +15,40 @@ data "aws_ami" "ubuntu" {
     values = ["hvm"]
   }
 
-  owners = ["099720109477"] # Canonical
+  owners = ["099720109477"]
 }
 
 data "aws_vpc" "default" {
   default = true
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow SSH and HTTP"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_key_pair" "web_key" {
@@ -24,12 +57,16 @@ resource "aws_key_pair" "web_key" {
 }
 
 resource "aws_instance" "ec2_webapp" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-
-  key_name = aws_key_pair.web_key.key_name
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.web_key.key_name
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   tags = {
     Name = "WebAppInstance"
   }
+}
+
+output "public_ip" {
+  value = aws_instance.ec2_webapp.public_ip
 }
---
+++ variable.tf
@@ -0,0 +1,4 @@
+variable "public_key" {
+  description = "SSH public key"
+  type        = string
+}
