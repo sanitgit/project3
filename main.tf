e57d18c26f4f12acd204dadedf322806eba18374
sanitgit <sanits212@gmail.com>
2026-03-12 13:43:01 +0800

    key pair

--- main.tf
+++ main.tf
@@ -1,25 +1,35 @@
-
 data "aws_ami" "ubuntu" {
-    most_recent = true
-    filter {
-        name   = "name"
-        values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-    }
-    filter {
-        name   = "virtualization-type"
-        values = ["hvm"]
-    }
-    owners = ["099720109477"] # Canonical
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_key_pair" "web_key" {
+  key_name   = "project4-key"
+  public_key = file("project4-key.pub")
 }
 
 resource "aws_instance" "ec2_webapp" {
-    ami                    = data.aws_ami.ubuntu.id
-    instance_type          = "t2.micro"
-    
-    tags = {
-        Name = "WebAppInstance"
-    }
-}
-data "aws_vpc" "default" {
-  default = true
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  key_name = aws_key_pair.web_key.key_name
+
+  tags = {
+    Name = "WebAppInstance"
+  }
 }
